273d3f25f35ed4609d8d3f618165998e1b3e814d
Maicon Crhistian <maiconcrhistian@MacBook-Pro-de-Maicon.local>
2021-02-21 14:30:29 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState,useEffect} from 'react';
+import { createServer } from 'miragejs';
+
+
+let server = createServer()
+server.get("/api/users", { users: [{ id: 1, name: "Bob" }] })
 
 function App() {
+  const [users,setUsers] = useState([]);
+
+  useEffect(()=>{
+    fetch("/api/users")
+    .then((res) => res.json())
+    .then((json) => {
+      setUsers(json.users)
+    })
+    
+  },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Hello World</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
   );
 }
 
